0726bef36a6ae1428bf8666f76f05534e53963a0
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-01 17:44:02 -0500

    Added properties to tomagotchi

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
-console.log('this working???');
+// console.log('this working???');
 
 class Tomagotchi {
+	constructor(age){
+		this.name = 'Mukimukitchi';
+		this.age = age;
+		this.hunger = Math.floor(Math.random()* 10) + 1;
+		this.sleepiness = Math.floor(Math.random()* 10) + 1;
+		this.boredom = Math.floor(Math.random()* 10) + 1;
 
-}
+	}
+};
 
 const game = {
+	time: 0
 
-}
-
+};
+	
 //button starts game
