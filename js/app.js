0219af732c1af73610361b113f0f72c6993efbbe
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-01 20:18:45 -0500

    Added more buttons

--- js/app.js
+++ js/app.js
@@ -1,19 +1,58 @@
 // console.log('this working???');
 
+
+
+
+
 class Tomagotchi {
-	constructor(age){
-		this.name = 'Mukimukitchi';
+	constructor(name, age){
+		this.name = name;
 		this.age = age;
-		this.hunger = Math.floor(Math.random()* 10) + 1;
-		this.sleepiness = Math.floor(Math.random()* 10) + 1;
-		this.boredom = Math.floor(Math.random()* 10) + 1;
+		this.hunger = 1;
+		this.sleepiness = 1;
+		this.boredom = 1;
+	};
+	// feed: function(){ // increase t hunger level
 
-	}
+	// };
+	// turnOffLights: function(){
+
+	// };
+	// playTime: function(){
+
+	// }
 };
-
+//I want the game to start after the user puts input
 const game = {
-	time: 0
+	time: 10,
+	start: function(){
+	const userInput = prompt(`Name your Tomagotchi`);
+	console.log(`Welcome ${userInput}`);
+	// I want to take users input and append it to the dom
+	// $(userInput).text();
+	this.setTimer()
+  },
+  setTimer: function(){
+  	const $timer = $('#timer');
+  	const interval = setInterval (() => {
+  		if (this.time === 0) {
+  		  clearInterval(interval);	
+  		} else {
+  		  Tomagotchi.age ++
+  		}
 
-};
-	
+  	$timer.text(`timer: ${this.time}s`)
+  }, 1000)
+}	
+}	
 //button starts game
+
+
+
+
+
+game.start();
+// game.setTimer();
+
+
+// Math.floor(Math.random() * 10) + 1
