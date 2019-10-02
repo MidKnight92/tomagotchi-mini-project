33663e20ad1e70cf8c10c88cc540cd9fd91887f8
sviveros05@gmail.com <sviveros05@gmail.com>
2019-10-02 10:38:34 -0500

    Timer is working

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 // console.log('this working???');
 
+//	------------------------- Buttons (Event Listners) -------------------------
 // This places an event listner on the "on" button and starts the game
 $('#on').on('click', (e) => {
 	game.start();
 });
 
+// This button will run the feed function
+$('#feed').on('click', (e) => {
+	//invoke feed function
+});
 
+// This button will run the turnOffLights function
+$('#lightsOff').on('click', (e) => {
+	//invoke function
+});
 
+// This button will run the playtime function
+$('#playTime').on('click', (e) => {
+	//invoke function
+});
+
+// ----------------  Tman and Game object below --------
 
 class Tomagotchi {
 	constructor(name, age){
@@ -24,7 +39,10 @@ class Tomagotchi {
 	// };
 	// playTime: function(){
 
-	// }
+	// };
+	// death: function(){
+
+	// };
 };
 const Tpet =  new Tomagotchi('tpet', 0);
 console.log(Tpet);
@@ -44,21 +62,22 @@ const game = {
 	this.setTimer()
   },
   // I want the timer to increment up 
-  	// as time increments up I want T's age to increment up
-  	
+  	// to call an age method that will increase the tomagotchis age ever interval
+  	// I want to invoke three different methods that will increase the hunger, sleepiness, and boredom at diffent times
     setTimer: function(){
   	const $timer = $('#timer');
   	const interval = setInterval (() => {
-  		if (this.time === 0) {
-  		  this.time ++ ;	
-  		  // Tpet(age)++
-  		} 
+  		this.time ++ 
+  		 console.log(Tpet['age']++);
 	  	$timer.text(`timer: ${this.time}s`)
 	  }, 1000)
 	}	
 }	
 
-
+// if (this.time === 0) {
+  		  	
+//   		//  
+//   		// }
 
 
 
